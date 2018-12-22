aeb15759c48772149fdab4f524dfd96e835b18d1
Jackipan <2466366246@qq.com>
2018-12-22 17:43:47 +0800

    click event

--- js/click.js
+++ js/click.js
@@ -2,7 +2,7 @@
 var a_idx = 0;
 jQuery(document).ready(function ($) {
     $("body").click(function (e) {
-        var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+        var a = new Array("♥", "徐", "大", "铭", "同", "学", "生", "日", "快", "乐", "啊", "♥", "还", "有", "圣", "诞", "也", "要", "快", "乐", "♥", "天", "天", "都", "要", "开", "心","哈", "♥");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
